3d948798eeb84172511b1e9b57aa75f723c0570c
LeeHanYeong <dev@lhy.kr>
2018-10-04 14:30:20 +0900

    ResultsView, VoteView구현

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -4,9 +4,11 @@ from django.urls import path, include
 from .views import cbv, fbv as views
 
 app_name = 'polls'
-
 urlpatterns_cbv = [
     path('', cbv.IndexView.as_view(), name='index'),
+    path('<int:question_id>/', cbv.DetailView.as_view(), name='detail'),
+    path('<int:question_id>/results/', cbv.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', cbv.VoteView.as_view(), name='vote'),
 ]
 urlpatterns = [
     # r'^$'
